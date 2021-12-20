79ca386413c5c0bdb371b9bc81f4327bc3538694
CarlVenter-Git <carlventer@live.co.za>
2021-12-20 22:13:40 +0200

    Add project files.

---
+++ Tempa/Client/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Tempa.Client;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("Tempa.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Tempa.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
